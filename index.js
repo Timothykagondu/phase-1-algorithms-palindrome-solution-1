be8c8c44d5373a78eddec445c339afd73f0dd8ab
Timothy <timothy.2k21@gmail.com>
2022-09-01 14:09:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord = reverseString(word);
+	return word === reversedWord;
 }
-
+function reverseString(word) {
+	return word.split("").reverse().join("");
+}
 /* 
   Add your pseudocode here
 */
